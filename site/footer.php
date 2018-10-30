38f0936846f97689b21a5ca702f694fb53b73653
Lucas Dev <lucas_ss.arts@live.com>
2018-10-30 14:59:58 -0300

    Subindo codigos

---
+++ site/footer.php
@@ -0,0 +1,70 @@
+        <div id="footer" class="row">
+            <div class="col-md-6">
+                <div class="row left-footer">
+                    <div class="col-md-4">
+                        <span>LOJAS LOGO</span>
+                        <ul>
+                            <li>
+                                <a href="#">Sobre</a>
+                            </li>
+                            <li>
+                                <a href="#">Lojas</a>
+                            </li>
+                            <li>
+                                <a href="#">Trabalhe conosco</a>
+                            </li>
+                            <li>
+                                <a href="#">Contato</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <span>LISTA DE ATALHOS</span>
+                        <ul>
+                            <li>
+                                <a href="#">Portal do Colaborador</a>
+                            </li>
+                            <li>
+                                <a href="#">Promoções</a>
+                            </li>
+                            <li>
+                                <a href="#">Cartão do Cliente</a>
+                            </li>
+                            <li>
+                                <a href="#">Cadastre-se</a>
+                            </li>
+                            <li>
+                                <a href="#">Blog</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <span>SAC LOJA LOGO</span>
+                        <span>0800-701-0316</span>
+                        <br>
+                        <a href="#">
+                            <img src="<?php bloginfo('template_directory'); ?>/btn-2-via-boleto.jpg">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 img_rodape">
+                <img src="<?php bloginfo('template_directory'); ?>/img-rodape.jpg">
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+            crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossorigin="anonymous"></script>
+        <script>
+            $(document).ready(function () {
+                $('.carousel').carousel({
+                    interval: 2000
+                })
+            })
+        </script>
+</body>
+
+</html>
---
+++ site/index.php
@@ -0,0 +1,119 @@
+    <?php get_header(); ?>
+    <div class="container-fluid">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <menu class="menu-secundario">
+                <ul>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">PROMOÇÕES</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">CASA</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">KIDS</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">MASCULINO</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">FEMININO</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">CAMPANHAS</a>
+                    </li>
+                </ul>
+            </menu>
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="<?php bloginfo('template_directory'); ?>/banner01.png" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="<?php bloginfo('template_directory'); ?>/banner02.png" alt="Second slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+        <?php get_sidebar(); ?>
+        
+        <div class="row">
+            <div class="offset-md-1 col-md-10 col-xs-12 imgs-dinamicas">
+                <div class="row-img-main">
+                    <img src="<?php bloginfo('template_directory'); ?>/img01.jpg">
+                </div>
+                <div class="row-img-1">
+                    <img src="<?php bloginfo('template_directory'); ?>/img02.jpg">
+                    <img src="<?php bloginfo('template_directory'); ?>/img03.jpg">
+                    <br>
+
+                    <img src="<?php bloginfo('template_directory'); ?>/img04.jpg">
+                    <img src="<?php bloginfo('template_directory'); ?>/img05.jpg">
+                </div>
+            </div>
+        </div>
+        <div class="row maps">
+            <div class="col-md-12">
+                <div class="mascara_maps"></div>
+                <div class="dados_mapa">
+                    <div class="col-md-2 btn_achar_minha_localizacao_automaticamente">
+                        <a href="#">
+                            <img src="<?php bloginfo('template_directory'); ?>/btn-localizacao-por-endereco.jpg">
+                        </a>
+                    </div>
+                    <div class="col-md-1 linha_separador_localizacao">
+                        <img src="<?php bloginfo('template_directory'); ?>/linha-separador-localizacao.jpg">
+                    </div>
+                    <div class="col-md-5 busca_por_cep">
+                        <label class="label_cep">DIGITE O CEP DE ONDE VOCÊ ESTÁ</label>
+                        <div class="caixa_campo_cep">
+                            <input type="text" name="cep" placeholder="CEP">
+                            <button type="button" name="btn_buscar_cep">PROCURAR</button>
+                        </div>
+                    </div>
+                </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3643.0077936923904!2d-46.566084584672275!3d-24.066030086308032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce20faa8fff8d1%3A0xc19255471ff841c3!2sJardim+Real%2C+Praia+Grande+-+State+of+S%C3%A3o+Paulo!5e0!3m2!1sen!2sbr!4v1540898001056"
+                    width="100%" height="355" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="row news_letter">
+            <div class="offset-md-2 col-md-5 col-xs-12 news_letter">
+                <form action="/news_letter" method="post">
+                    <label class="news_letter">ASSINE A NEWSLETTER DO LOGO</label>
+                    <br/>
+                    <div class="caixa_campo">
+                        <input type="email" name="email_newsletter" placeholder="Seu e-mail">
+                        <button type="submit" name="btn_newsletter">ENVIAR</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-4 col-xs-12 redes_sociais">
+                <span>SIGA LOJAS LOGO NAS REDES SOCIAIS</span>
+                <br/>
+                <ul>
+                    <li>
+                        <img src="<?php bloginfo('template_directory'); ?>/icon-facebook.jpg">
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_directory'); ?>/icon-youtube.jpg">
+                    </li>
+                    <li>
+                        <img src="<?php bloginfo('template_directory'); ?>/icon-instagram.jpg">
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+    </div>
+    <?php get_footer(); ?>
